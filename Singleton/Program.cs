089255c9ed43a8512491e4f6ec51e52570fd5714
mohammad <mohammad.zr1378@gmail.com>
2024-01-11 16:14:16 +0330

    Singleton design pattern implemented

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿var singletonInstance = Singleton.GetInstance();
+var anotherSingletonInstance = Singleton.GetInstance();
+
+Console.WriteLine(singletonInstance == anotherSingletonInstance);
+
+
+public class Singleton
+{
+    private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+    private Singleton()
+    {
+
+    }
+
+    public static Singleton GetInstance()
+    {
+        return _instance.Value;
+    }
+}
